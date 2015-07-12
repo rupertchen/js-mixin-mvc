637751cbd9445d2fa1f70f64099078ddc6a61d08
Rupert Chen <rupert.chen@gmail.com>
2015-07-12 00:06:58 -0700

    Cleanup concat and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,25 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      build: {
-        src: [
-          'src/a.js',
-          'src/b.js',
-          'src/js-mixin-mvc.js'
-        ],
-        dest: 'build/<%= pkg.name %>.js',
-        nonull: true
-      }
-    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         report: 'min'
       },
       build: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        options: {
+          sourceMap: true,
+          mangle: false
+        },
+        files: {
+          'build/<%= pkg.name %>.min.js': ['src/**/*.js']
+        }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['uglify']);
 };
 
