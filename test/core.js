183d79140371e344ab82aca038c31a5a435232d2
Rupert Chen <rupert.chen@gmail.com>
2015-07-12 03:54:00 -0700

    Add pending facade tests

---
+++ test/core.js
@@ -0,0 +1,21 @@
+describe('Facade', function () {
+  describe('proxy', function () {
+    it('can register');
+    it('can retrieve');
+    it('can check existence');
+    it('can remove');
+  });
+  describe('mediator', function () {
+    it('can register');
+    it('can retrieve');
+    it('can check existence');
+    it('can remove');
+  });
+  describe('command', function () {
+    it('can register');
+    it('can retrieve');
+    it('can check existence');
+    it('can remove');
+  });
+  it('can send notifications');
+});
